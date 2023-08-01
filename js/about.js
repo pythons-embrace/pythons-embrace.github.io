ba46791bbf31fea16bdca8e1554017547e04114b
Chas Nelson <github@chasnelson.co.uk>
2023-08-01 15:28:40 +0100

    feat: update homepage and componentise certain features

---
+++ js/about.js
@@ -0,0 +1,77 @@
+class About extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <section id='about' class="s-about">
+
+        <div class="row section-header has-bottom-sep" data-aos="fade-up">
+            <div class="col-full">
+                <h3 class="subhead subhead--dark">Hello There</h3>
+                <h1 class="display-1 display-1--light">Embrace Python and embrace better science</h1>
+            </div>
+        </div> <!-- end section-header -->
+
+        <div class="row about-desc" data-aos="fade-up">
+            <div class="col-full">
+                <p>Python's Embrace is the work of Dr Chas Nelson and Miks Kundegorski - one biologist who found themselves in computing; and one computer scientist who found themselves in a field! We are passionate about sharing our knowledge and experience with others and helping them to develop their skills in Python and data science.
+                </p>
+            </div>
+        </div> <!-- end about-desc -->
+  
+        <div class="row about-desc block-1-2 block-tab-full">
+
+            <div class="col-block service-item" data-aos="fade-up">
+                <div class="about-avatar">
+                    <img src="../images/avatars/cjn.jpg">
+                </div>
+                <div class="about-bio">
+                    <h3 class="h2">Dr Chas Nelson</h3>
+                    <p><a href="chasnelson.co.uk">Dr Chas Nelson</a> is an International Data Consultant working with Fjelltopp Ltd and various UN Organisations. Chas has taught computer science topics to a wide variety of groups and comes prepared with an undergraduate degree in biology (and physics) and a PhD in computer science, specifically bioimage analysis. Chas uses Python daily and has an interest in building courses that enable participants to go on and continue learning after the course.
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-block service-item" data-aos="fade-up">
+                <div class="about-avatar">
+                    <img src="../images/avatars/mixmixmix.png">
+                </div>
+                <div class="about-bio">
+                    <h3 class="h2">Miks Kundegorski</h3>
+                    <p><a href="mixmixmix.github.io">Mikolaj (Miks) Kundegorski</a> is a Software Engineer at Fjelltopp Ltd and Health Informatics Consultant with the UN Food and Agricultural Organisation. His background spans from physics to computer vision and he is currently working towards his PhD in modelling of collective animal behaviour at the University of Glasgow. He uses Python to solve a wide variety of problems in international development and academia and is passionate about sharing his expertise.
+                    </p>
+                </div>
+            </div>
+
+        </div><!-- end services-list -->
+
+        <div class="row about-desc" data-aos="fade-up">
+            <div class="col-full">
+                <p>Between them, Chas and Miks will be able to fully support your learning throughout the course and provide you with the tools to continue developing your skills after the course.
+                </p>
+            </div>
+        </div> <!-- end about-desc -->
+  
+        <div class="row about-stats stats block-1-3 block-m-1-2 block-mob-full" data-aos="fade-up">
+                
+            <div class="col-block stats__col ">
+                <div class="stats__count">21</div>
+                <h5>Courses Run</h5>
+            </div>
+            <div class="col-block stats__col">
+                <div class="stats__count">1034</div>
+                <h5>New Pythonistas</h5> 
+            </div>
+            <div class="col-block stats__col">
+                <div class="stats__count">1990</div>
+                <h5>Cups of Coffee</h5>
+            </div>
+  
+        </div> <!-- end about-stats -->
+  
+        <div class="about__line"></div>
+  
+    </section> <!-- end s-about -->
+          `;
+  }
+}
+
+customElements.define('about-us-section', About);
---
+++ js/footer.js
@@ -0,0 +1,51 @@
+class Footer extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+      <footer>
+
+      <div class="row footer-main">
+
+          <div class="col-six tab-full left footer-desc">
+
+              <div class="footer-logo">Python's Embrace</div>
+
+          </div>
+
+          <div class="col-six tab-full right footer-subscribe">
+
+              <h4>Our Courses</h4>
+
+              <ul class="center-wrapped">
+                <li><a href="/courses/beginners.html">Python for Biologists</a></li>
+                <li><a href="/courses/data_exploration.html">Data Explorations</a></li>
+                <li><a href="/courses/image_analysis.html">Image Analysis</a></li>
+                <li><a href="/courses/machine_learning.html">Machine Learning</a></li>
+                <li><a href="/courses/workflows.html">Data Workflows</a></li>
+              </ul>
+
+          </div>
+
+      </div> <!-- end footer-main -->
+
+      <div class="row footer-bottom">
+
+          <div class="col-twelve">
+              <div class="copyright">
+                  <span>© Copyright Chas Nelson 2023</span> 
+                  <span>Site Template by <a href="https://www.colorlib.com/">Colorlib</a></span>	
+              </div>
+
+              <div class="go-top">
+                  <a class="smoothscroll" title="Back to Top" href="#top"><i class="icon-arrow-up" aria-hidden="true"></i></a>
+              </div>
+          </div>
+
+      </div> <!-- end footer-bottom -->
+
+  </footer> <!-- end footer -->
+            `;
+    }
+  }
+  
+  customElements.define('footer-section', Footer);
+  
